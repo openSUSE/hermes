ad3243cda59d62e6e94e86c96d43cc0d6bf51176
Klaas Freitag <freitag@suse.de>
2008-04-25 10:11:34 +0000

    Usage added, small bugfixes

--- hermesworker.pl
+++ hermesworker.pl
@@ -23,25 +23,41 @@
 # This script diggs through the hermes database and sends the due
 # messages, sleeping again for a short time. 
 
-use strict qw 'vars';
+use strict;
 use Getopt::Std;
+
 use Hermes::MessageSender;
+use Hermes::Message;
 use SDBM_File;
 use Time::HiRes qw( gettimeofday tv_interval );
 use Fcntl;
-
-use vars qw ( $opt_h $opt_s $opt_d $opt_w $opt_t );
+use Hermes::Log;
+use vars qw ( $opt_h $opt_s $opt_d $opt_w $opt_t $opt_o $opt_m );
 
 
 sub usage()
 {
   print "Usage!";
+
+  print<<END
+
+  hermesworker.pl
+
+  Script to send out all kinds of hermes messages.
+
+  -o:  send only immediate messages once and stop after that
+  -m:  send only minute digests and stop after that
+  -h:  help text
+  -d:  switch on debug
+END
+;
+  exit;
 }
 
 # ---------------------------------------------------------------------------
 
 # Process the commandline arguments.
-getopts('w:dhst:');
+getopts('omw:dhst:');
 
 usage() if ($opt_h );
 
@@ -55,6 +71,7 @@ $debug = 1 if( $opt_d );
 my $type;
 $type = $opt_t if( $opt_t );
 
+
 # Sending time for daily digests, defaults to midnight
 my $dailyHour = $Hermes::Config::DailySendHour || 0;
 my $dailyMin  = $Hermes::Config::DailySendHourMinute || 0;
@@ -62,7 +79,7 @@ my $dailyMin  = $Hermes::Config::DailySendHourMinute || 0;
 my $timerfile = "workertimes";
 my %times;
 
-tie( %times, SDBM_File, $timerfile, O_RDWR|O_CREAT, 0644 ) || die "Cannot open timer file $timerfile\n";
+# tie( %times, SDBM_File, $timerfile, O_RDWR|O_CREAT, 0644 ) || die "Cannot open timer file $timerfile\n";
 
 for my $step ( ('minute', 'hour', 'week', 'month') ) {
     $times{ $step } = 0 unless( exists $times{ $step } );
@@ -73,6 +90,15 @@ my $workerdelay = $opt_w || 10;
 my ($t0, $elapsed);
 my $cnt;
 
+if( $opt_m ) {
+    $cnt = 0+sendMessageDigest( SendMinutely() );
+    print "Sent $cnt messages (minute digests)\n";
+    for( my $i = 0; $i < 20; $i++ ) {
+	log( 'info', "Filler $i" );
+    }
+    exit;
+}
+
 while( 1 ) {
     my ($sec,$min,$hour,$mday,$mon,$year,$wday,$yday,$isdst) = localtime(time);
 
@@ -80,34 +106,41 @@ while( 1 ) {
     $t0 = [gettimeofday];
     $cnt = sendImmediateMessages();
     $elapsed = tv_interval ($t0);
-    print "Sent due messages: $cnt in $elapsed sec.\n";
+    log 'info', "Sent due messages: $cnt in $elapsed sec.\n";
+    print "Sent immediate due messages: $cnt in $elapsed sec.\n";
     
+    exit if( $opt_o );
+
     # send minute digests if $sec >= 0 AND $sec < $workerdelay
     if( $sec >= 0 && $sec < $workerdelay ) {
 	
 	$t0 = [gettimeofday];
-	$cnt = sendMessageDigest( SendMinutely );
+	$cnt = sendMessageDigest( SendMinutely() );
 	$elapsed = tv_interval( $t0 );
+	log 'info', "Sent Minute digest at <$min/$sec>: $cnt in $elapsed sec.";
 	print "Sent Minute digest at <$min/$sec>: $cnt in $elapsed sec.\n";
 
 	if( $min == 0 ) {
-	    print "Send Hour digest at <$hour/$min/$sec>\n";
+	    log 'info', "Send Hour digest at <$hour/$min/$sec>\n";
 	    $t0 = [gettimeofday];
-	    $cnt = sendMessageDigest( SendHourly );
+	    $cnt = sendMessageDigest( SendHourly() );
 	    $elapsed = tv_interval($t0);
+	    log 'info', "Sent Minute digest at <$min/$sec>: $cnt in $elapsed sec.";
 	    print "Sent Minute digest at <$min/$sec>: $cnt in $elapsed sec.\n";
 
 	}
 
 	if( $hour == $dailyHour && $min == $dailyMin ) {
 	    $t0 = [gettimeofday];
-	    $cnt = sendMessageDigest( SendDaily );
+	    $cnt = sendMessageDigest( SendDaily() );
 	    $elapsed = tv_interval($t0);
+	    log 'info', "Send Daily Digest at <$hour/$min/$sec>: $cnt in $elapsed sec.";
 	    print "Send Daily Digest at <$hour/$min/$sec>: $cnt in $elapsed sec.\n";
 	}
     }
 
     print "* Now sleeping for $workerdelay seconds\n";
+    log( 'info', "Sleeping for $workerdelay seconds" );
     sleep( $workerdelay );
 }
 
