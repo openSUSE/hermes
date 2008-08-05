dccfa0cb833f878fc3d04fc8798283191a0e331c
Tom Patzig <tom@tomstar.org>
2008-08-05 16:58:02 +0000

    -set the filter value for the special operator, depending on the parameter selection

--- starship/app/models/msg_types_people.rb
+++
@@ -1,7 +0,0 @@
-class MsgTypesPeople < ActiveRecord::Base
-  set_table_name "msg_types_people"
-  belongs_to :msg_type
-  belongs_to :person
-  belongs_to :delay
-  belongs_to :delivery 
-end
--- starship/app/models/subscription.rb
+++ starship/app/models/subscription.rb
@@ -7,4 +7,7 @@ class Subscription < ActiveRecord::Base
 
   has_many :messages, :foreign_key => :msg_type_id
   has_many :filters, :class_name => "SubscriptionFilter", :dependent => :destroy
+
+  validates_presence_of :delay
+  validates_presence_of :delivery
 end
