77833e568da47dad93a532d0a8b939f38908c198
Tom Patzig <tom@tomstar.org>
2008-08-06 14:04:29 +0000

    -save the last entered filter value -handle the observer overflow

--- starship/public/javascripts/application.js
+++ starship/public/javascripts/application.js
@@ -25,9 +25,11 @@ function recalc_filter_ids() {
     filter.adjacent('input.filter_value_input')[i].id = 'filter_value_'+i;
 
     param_select = filter.adjacent('select.param_select')[i];
-    new Form.Element.Observer( param_select.id, 0 ,function(param_select, value) {
-      special_operator_checks(param_select.id);
-    });
+    
+    //delete the old Observer and create a new one
+    stop_observer(param_select);
+    add_observer(param_select);
+
     special_operator_checks(param_select);
   }
 }
@@ -37,7 +39,6 @@ function filter_count() {
   
 }
 
-
 //observer for the special operator (if parameter == package || parameter == project)
 function set_special_operator_observer() {
 
@@ -46,23 +47,27 @@ function set_special_operator_observer() {
   param_sel = "param_id_" + current_id;
 
   old_operator_hash.set(operator_element,$(operator_element).value);
-  old_value_hash.set(operator_element, $('filter_value_' + current_id).value);
+  
+  add_observer(operator_element);
 
-  new Form.Element.Observer(operator_element, 0 ,function(operator_element, value) {
-    special_operator_checks(operator_element);
+}
+
+function stop_observer(element) {
+  Event.stopObserving(element,'change',function(event) {
+    pecial_operator_checks(Event.element(event))
   });
+}
 
-  //observer for the Parameter
-  new Form.Element.Observer(param_sel, 0 ,function(param_sel, value) {
-    special_operator_checks(param_sel);
+function add_observer(element) {
+  Event.observe(element,'change',function(event) {
+      special_operator_checks(Event.element(event)) 
   });
-
 }
 
 //check if the parameter is package or project to fix the filter value input
 function special_operator_checks(element) {
-  
-  
+
+//  alert(element.id);
   id = $(element).name.match(/.$/);
 
   param_elem = $('param_id_' + id);
@@ -72,18 +77,27 @@ function special_operator_checks(element) {
   old_oper_value = old_operator_hash.get(operator_elem.id);
 
   
-  if( old_oper_value != 'special' &&  operator_elem.value == 'special') {
+  if( (old_oper_value != 'special' || old_oper_value == 'special'  ) &&  operator_elem.value == 'special') {
+//  if( old_oper_value != 'special' &&  operator_elem.value == 'special') {
     if(param_elem.options[param_elem.selectedIndex].text == 'package') {
+      if (old_oper_value != 'special')
+        old_value_hash.set(operator_element, $('filter_value_' + id).value);
       filter_elem.value =  '_mypackages';
       filter_elem.readOnly = true;
     } else if(param_elem.options[param_elem.selectedIndex].text == 'project') {
+      if (old_oper_value != 'special')
+        old_value_hash.set(operator_element, $('filter_value_' + id).value);
       filter_elem.value =  '_myprojects';
       filter_elem.readOnly = true;
     } 
 
   } else if(old_oper_value == 'special' && operator_elem.value != 'special' ){ 
-      filter_elem.value =  old_value_hash.get(operator_elem.id);
+      if (old_value_hash.get(operator_elem.id) != '')
+        filter_elem.value =  old_value_hash.get(operator_elem.id);
+      else
+        filter_elem.value = '';
       filter_elem.readOnly = false;
   }
+  old_operator_hash.set(operator_elem.id,operator_elem.value);
 }
 
