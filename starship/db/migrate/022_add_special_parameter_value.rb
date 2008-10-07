be508d721754434a8b5d8d6ca368041ae3230beb
Tom Patzig <tom@tomstar.org>
2008-10-07 17:31:36 +0000

    -update all existing special filters to new _special parameter

--- starship/db/migrate/022_add_special_parameter_value.rb
+++ starship/db/migrate/022_add_special_parameter_value.rb
@@ -1,10 +1,27 @@
 class AddSpecialParameterValue < ActiveRecord::Migration
   def self.up
-   Parameter.create :name => '_special'
+    Parameter.create :name => '_special'
+
+    #change the parameter for all existing filters with operator = special
+    filter = SubscriptionFilter.find (:all, :conditions => { :operator => 'special'})
+
+    filter.each { |f|
+      f.parameter = Parameter.find(:first, :conditions => {:name => '_special'})
+      f.save
+    }
+  
   end
 
   def self.down
     param = Parameter.find(:first, :conditions => { :name => '_special' } )
     param.destroy
+
+    filter = SubscriptionFilter.find (:all, :conditions =>{ :operator => 'special'})
+
+    filter.each { |f|
+      f.parameter = Parameter.find(:first) 
+      f.save
+    }
+
   end
 end
