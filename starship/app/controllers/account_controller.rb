ca051e81a8eede1f65bde730939c8076516da759
Thomas Schmidt <tom@opensuse.org>
2008-10-14 17:51:10 +0000

    added logout method

---
+++ starship/app/controllers/account_controller.rb
@@ -0,0 +1,13 @@
+class AccountController < ApplicationController
+  skip_before_filter :require_auth, :set_return_to
+
+  def login
+    auth_url = "https://hermes.opensuse.org/ICSLogin/?\"https://hermes.opensuse.org" + url_for(session[:return_to] || '/') + "\""
+    redirect_to(auth_url)
+  end
+
+  def logout
+    reset_session
+    redirect_to("https://hermes.opensuse.org/cmd/ICSLogout/")
+  end
+end
