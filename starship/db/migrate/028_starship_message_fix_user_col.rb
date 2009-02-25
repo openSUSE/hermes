0a801f1221acbdf09a4e327e2b50a0a31fcadb92
Andreas Bauer <abauer@suse.com>
2009-02-25 11:33:45 +0000

    - fixed broken migration

--- starship/db/migrate/028_starship_message_fix_user_col.rb
+++ starship/db/migrate/028_starship_message_fix_user_col.rb
@@ -1,11 +1,11 @@
 class StarshipMessageFixUserCol < ActiveRecord::Migration
   def self.up
     rename_column :starship_messages, :user, :user_id
-    change_column :starship_messages, :user_id, :integer 
+    change_column :starship_messages, :user_id, :integer, :default => 1 
   end
 
   def self.down
-    change_column :starship_messages, :user_id, :string
+    change_column :starship_messages, :user_id, :string, :default => ''
     rename_column :starship_messages, :user_id, :user
   end
 end
