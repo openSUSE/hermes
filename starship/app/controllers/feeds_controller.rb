7a746aae6d52bdf24be73429c3f61481ec4cd06b
Thomas Schmidt <tom@opensuse.org>
2009-03-05 18:31:23 +0000

    started working on the feeds

--- starship/app/controllers/feeds_controller.rb
+++ starship/app/controllers/feeds_controller.rb
@@ -1,5 +1,5 @@
 class FeedsController < ApplicationController
-  skip_before_filter :authenticate
+  skip_before_filter :authenticate, :only => ["personal"]
 
   def personal
     user = Person.find_by_stringid params[:person]
@@ -10,4 +10,31 @@ class FeedsController < ApplicationController
 
     @items = user.starship_messages.find :all, :limit => 100, :order => "created desc"
   end
+
+
+  # We could also show the genaral feeds here, also for non-logged in people
+  def index
+    # TODO: maybe we want mail delivered subscriptions also to be archived and viewable here?
+    # TODO: How do I put the select{} into the find() ?
+    @feed_subscriptions = session[:user].subscriptions.find( 
+      :all, :include => [:msg_type,:delay,:delivery]).select{|s| s.delivery.name = 'RSS' }     
+  end
+
+
+  # shows a feed either as RSS or web list. 
+  def show
+    @subscription = Subscription.find(:all, :conditions => ["id =?", params[:id]]).first
+    if (@subscription.nil?)
+      flash[:error] = "Feed with id: #{params[:id]} not found"
+      redirect_to :action => :index
+     elsif (@subscription.private && @subscription.person != session[:user])
+      flash[:error] = "Feed with id: #{params[:id]} is marked as private by it's owner"
+      redirect_to :action => :index
+    end
+    
+    
+  end
+
+
+
 end
