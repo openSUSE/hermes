12dfb1681733003ccf3a0d15da48564e60413a5d
Thomas Schmidt <tom@opensuse.org>
2009-03-08 16:09:12 +0000

    We have the following url schema now:
    /feeds (list public feeds)
    /feeds/personal (list your own feeds)
    /feeds/<username>{.rdf|.html} (all your feeds merged to one)
    /feeds/<subscription-id>{.rdf|.html} (a single feed)
    /feeds/<id>,<id>,<id>{.rdf|.html} (merge of given feeds)

--- starship/config/routes.rb
+++ starship/config/routes.rb
@@ -1,17 +1,21 @@
 ActionController::Routing::Routes.draw do |map|
+  
+  map.connect '', :controller => 'subscriptions', :action => 'simple'
+  
   map.resources :messages
   map.resources :msg_types
  
   map.connect '/subscriptions/simple', :controller => 'subscriptions', :action => 'simple'
   map.resources :subscriptions
 
-  map.connect '/', :controller => 'subscriptions', :action => 'simple'
+  map.connect '/feeds/:id.:format', :controller => 'feeds', :action => 'show', :requirements => { :id => /[\d,]+/ }
+  map.connect '/feeds/:person.:format', :controller => 'feeds', :action => 'person'
 
+  # TODO: add route for public + named feeds
+ 
   map.connect '/login',   :controller => 'account', :action => 'login'
   map.connect '/logout',  :controller => 'account', :action => 'logout'
   
-  map.connect '/feeds/:person/:action.:format', :controller => 'feeds'
-
   map.connect ':controller/:action/:id'
   map.connect ':controller/:action/:id.:format'
   
