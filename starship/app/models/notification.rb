3b3a47d1850721b8d897b4eb952b6ac8e8e42cd0
Thomas Schmidt <tom@opensuse.org>
2009-09-07 16:40:05 +0000

    fix relations

--- starship/app/models/notification.rb
+++ starship/app/models/notification.rb
@@ -1,5 +1,6 @@
 class Notification < ActiveRecord::Base
   has_many :notification_parameters, :dependent => :destroy
+  has_many :starship_messages
   belongs_to :msg_type
   belongs_to :generated_notification
 end
--- starship/app/models/starship_message.rb
+++ starship/app/models/starship_message.rb
@@ -1,5 +1,5 @@
 class StarshipMessage < ActiveRecord::Base
-
+  belongs_to :notification
   belongs_to :subscription
   belongs_to :msg_type
   belongs_to :msg_state
