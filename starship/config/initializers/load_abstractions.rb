ceff5aaae3ef187b7397a24b24701086b98d99f9
Klaas Freitag <freitag@suse.de>
2011-01-21 12:00:42 +0100

    - changed the required rails version to 2.3.8 - ported to use libxml instead of smart xml to read the abstractions.

--- starship/config/initializers/load_abstractions.rb
+++ starship/config/initializers/load_abstractions.rb
@@ -1,5 +1,7 @@
 ## parse xml files from config/guiabstractions
-require 'xml/smart'
+gem 'libxml-ruby', '>= 0.8.3'
+require 'xml'
+
 
 # hashes with key: element-id, value object
 abstractions = Hash.new
@@ -10,23 +12,27 @@ abstractiongroups = Hash.new
 if (ENV['RAILS_ENV'])
   Dir["#{RAILS_ROOT}/config/guiabstractions/*.xml"].each { |file|
     begin 
-      xml = XML::Smart.open(file)
       
-      xml.root.find("//filterabstract").each do | node | 
+      parser = XML::Parser.file( file )
+                                                         
+      doc = parser.parse
+      
+      doc.find("//filterabstract").each do | node | 
         filter_abstraction = FilterAbstract.new()
-        filter_abstraction.id = node.find("@id").first.value
-        filter_abstraction.summary = node.find("summary").first.text
-        filter_abstraction.description = node.find("description").first.text
+        filter_abstraction.id = node["id"]
+                                                         
+        filter_abstraction.summary = node.find_first("summary").content
+        filter_abstraction.description = node.find_first("description").content
         filter_abstraction.filters = Array.new
         node.find("filter").each do | filternode |
           filter = SubscriptionFilter.new
-          filter.operator = filternode.find("@operator").first.value
-          filter.filterstring = filternode.find("@value").first.value
+          filter.operator = filternode["operator"]
+          filter.filterstring = filternode["value"]
           db_parameter = Parameter.find(:all, 
-                                        :conditions => "name='" + filternode.find("@parameter").first.value + "'").first
+                                        :conditions => "name='" + filternode["parameter"]+ "'").first
           if (db_parameter.nil?)
             db_parameter = Parameter.new
-            db_parameter.name = filternode.find("@parameter").first.value
+            db_parameter.name = filternode["parameter"]
             db_parameter.save
             puts "Created parameter #{db_parameter.name} because it's used in an abstraction."
           end
@@ -36,18 +42,18 @@ if (ENV['RAILS_ENV'])
         filterabstractions[filter_abstraction.id] = filter_abstraction
       end
 
-      xml.find("//group").each do | node | 
-        group_id = node.find("@id").first.value
-        abstractiongroups[group_id] = node.find("name").first.text
+      doc.find("//group").each do | node | 
+        group_id = node["id"]
+        abstractiongroups[group_id] = node.find_first("name").content
         abstractions[group_id] = Hash.new
         
         subscounter = 0
 
         node.find("subscription").each do | subscription_node | 
           abstraction = SubscriptionAbstract.new()
-          abstraction.summary = subscription_node.find("summary").first.text
-          abstraction.description = subscription_node.find("description").first.text
-          abstraction.msg_type = subscription_node.find("msg_type/@name").first.value
+          abstraction.summary = subscription_node.find_first("summary").content
+          abstraction.description = subscription_node.find_first("description").content
+          abstraction.msg_type = subscription_node.find_first("msg_type")["name"]
           abstraction.sort_key = subscounter += 1
 
           if ((msg_type = MsgType.find(:first, :conditions => "msgtype =  '#{abstraction.msg_type}'")).nil?)
@@ -57,12 +63,12 @@ if (ENV['RAILS_ENV'])
             puts "Created msg_type #{abstraction.msg_type} because it's used in an abstraction."
           end
           msg_type.save
-          abstraction.id = subscription_node.find("@id").first.value
+          abstraction.id = subscription_node["id"]
           abstraction.filterabstracts = Hash.new
           
           #add filter abstractions
           subscription_node.find("checkable").each do | checkable_node |
-            filterabstract_name = checkable_node.find("@filterabstract").first.value
+            filterabstract_name = checkable_node["filterabstract"]
             abstraction.filterabstracts[filterabstract_name] = filterabstractions[filterabstract_name]
             # add connection msg_type <-> parameter
             filterabstractions[filterabstract_name].filters.each do |subsfilter|
