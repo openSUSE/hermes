af84f0bc722779542c7ad7b38e3e9474cc2b1774
Thomas Schmidt <tschmidt@suse.de>
2011-06-01 15:36:21 +0200

    pre-order values for graph, add description text

--- starship/app/controllers/statistics_controller.rb
+++ starship/app/controllers/statistics_controller.rb
@@ -4,8 +4,8 @@ class StatisticsController < ApplicationController
     @users = Person.find :all
     @subscriptions = Subscription.find :all
     cacheid = "hermes_stats"
-    @timespan = 744
-    @notifications_in, @notifications_out, @messages = Rails.cache.fetch(cacheid, :expires_in => 2.seconds) do
+    @timespan = 7*24
+    @notifications_in, @notifications_out, @messages = Rails.cache.fetch(cacheid, :expires_in => 1.hour) do
       notifications_in = get_notification_rows "notifications", "generated", @timespan
       notifications_out = get_notification_rows "generated_notifications", "sent", @timespan
       messages = get_notification_rows "starship_messages", "created", @timespan
@@ -22,7 +22,9 @@ class StatisticsController < ApplicationController
       SELECT round((unix_timestamp( #{date_column} ) - unix_timestamp( NOW() ) ) / 3600), COUNT( #{date_column} )
       FROM #{table}
       WHERE #{date_column} > "#{timespan.hours.ago.to_s(:db)}"
+      AND #{date_column} < "#{Time.now.strftime("%Y-%m-%d %H:00") }"
       GROUP BY DATE_FORMAT( #{date_column}, "%d-%m-%Y %H" )
+      ORDER BY #{date_column}
     END_SQL
     rows.each {|row| notifications << row}
     notifications
