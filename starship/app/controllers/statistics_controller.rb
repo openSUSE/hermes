9242f201470edca2307f76b3f11150f53039bd49
Thomas Schmidt <tschmidt@suse.de>
2011-06-10 17:04:23 +0200

    allow to select multiple graphs

--- starship/app/controllers/statistics_controller.rb
+++ starship/app/controllers/statistics_controller.rb
@@ -17,12 +17,11 @@ class StatisticsController < ApplicationController
 
   def msg_type
     @timespan = 7*24
-    notifications_in = get_notification_rows "notifications", "received", @timespan
     @all_msg_types = MsgType.find(:all, :order => 'description, msgtype DESC')
     @msg_types = params['msg_type']
     @notifications_in_by_type = {}
-    @all_msg_types.each do |type|
-      @notifications_in_by_type[type.id] = notifications_in.select{|time, n, t| @msg_types.include? t} if @msg_types.include? type.id.to_s
+    @msg_types.each do |type_id|
+      @notifications_in_by_type[type_id] = get_msgtype_rows type_id, @timespan
     end
   end
 
@@ -33,12 +32,27 @@ class StatisticsController < ApplicationController
     notifications = []
     rows = ActiveRecord::Base.connection.execute <<-END_SQL
       SELECT ceil((unix_timestamp( #{date_column} ) - unix_timestamp( NOW() ) ) / 3600), COUNT( #{date_column} )
-      #{", msg_type_id" if table == 'notifications'}
       FROM #{table}
       WHERE #{date_column} > "#{timespan.hours.ago.to_s(:db)}"
       AND #{date_column} < "#{Time.now.strftime("%Y-%m-%d %H:00") }"
       GROUP BY DATE_FORMAT( #{date_column}, "%d-%m-%Y %H" )
       ORDER BY #{date_column}
+    END_SQL
+    rows.each {|row| notifications << row}
+    notifications
+  end
+
+
+  def get_msgtype_rows msgtype_id, timespan
+    notifications = []
+    rows = ActiveRecord::Base.connection.execute <<-END_SQL
+      SELECT ceil((unix_timestamp( received ) - unix_timestamp( NOW() ) ) / 3600), COUNT( received ), msg_type_id
+      FROM notifications
+      WHERE received > "#{timespan.hours.ago.to_s(:db)}"
+      AND received < "#{Time.now.strftime("%Y-%m-%d %H:00") }"
+      AND msg_type_id = #{msgtype_id}
+      GROUP BY DATE_FORMAT( received, "%d-%m-%Y %H" )
+      ORDER BY received
     END_SQL
     rows.each {|row| notifications << row}
     notifications
