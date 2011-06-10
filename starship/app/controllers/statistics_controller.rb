554c312b55717ab43cdf8696bb716a63a00d496f
Thomas Schmidt <tschmidt@suse.de>
2011-06-10 13:44:29 +0200

    add page for message type graphs

--- starship/app/controllers/statistics_controller.rb
+++ starship/app/controllers/statistics_controller.rb
@@ -1,16 +1,27 @@
 class StatisticsController < ApplicationController
 
   def index
-
     @users = Person.count
     @subscriptions = Subscription.count
     cacheid = "hermes_stats"
     @timespan = 7*24
-    @notifications_in, @notifications_out, @messages = Rails.cache.fetch(cacheid, :expires_in => 1.hour) do
+    @notifications_in, @notifications_out, @messages = Rails.cache.fetch(cacheid, :expires_in => 30.minutes) do
       notifications_in = get_notification_rows "notifications", "received", @timespan
       notifications_out = get_notification_rows "generated_notifications", "sent", @timespan
       messages = get_notification_rows "starship_messages", "created", @timespan
       [notifications_in, notifications_out, messages]
+    end
+    @msg_types = MsgType.find(:all, :order => 'description, msgtype DESC')
+  end
+
+  def msg_type
+    @timespan = 7*24
+    notifications_in = get_notification_rows "notifications", "received", @timespan
+    @all_msg_types = MsgType.find(:all, :order => 'description, msgtype DESC')
+    @msg_types = params['msg_type']
+    @notifications_in_by_type = {}
+    @all_msg_types.each do |type|
+      @notifications_in_by_type[type.id] = notifications_in.select{|time, n, t| @msg_types.include? t} if @msg_types.include? type.id.to_s
     end
   end
 
@@ -20,7 +31,8 @@ class StatisticsController < ApplicationController
   def get_notification_rows table, date_column, timespan
     notifications = []
     rows = ActiveRecord::Base.connection.execute <<-END_SQL
-      SELECT round((unix_timestamp( #{date_column} ) - unix_timestamp( NOW() ) ) / 3600), COUNT( #{date_column} )
+      SELECT ceil((unix_timestamp( #{date_column} ) - unix_timestamp( NOW() ) ) / 3600), COUNT( #{date_column} )
+      #{", msg_type_id" if table == 'notifications'}
       FROM #{table}
       WHERE #{date_column} > "#{timespan.hours.ago.to_s(:db)}"
       AND #{date_column} < "#{Time.now.strftime("%Y-%m-%d %H:00") }"
