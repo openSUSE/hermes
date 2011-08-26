cf9657ae6447570433911cb337bbadb60fe24ac5
Stephan Kulow <coolo@suse.de>
2011-08-26 07:15:50 +0200

    Make digests more reliable
    Instead of checking 1000 messages max once a day, we now grab all
    digests once a day and then send one digest after sending due messages.
    And if the digest has a lot of mails (at least 3 people are subscribed
    to build_success PER_WEEK, which are a LOT of emails), we send several
    mails - 300 per digest.
    
    This makes sure we don't forget digests, the current downside is that
    we send digests of all kind if hermesworker restarts. If people complain
    (if we happen to restart several times a day PER_WEEK becomes useless)
    we need to either have a timestamp in file system or database. But that
    is not as simple either as just because we grabed digests doesn't mean
    the digests are sent out. One possible measure is to save per (digest)
    subscribtion when the last mail was sent out and use that to limit how
    often we regrab it.

--- Hermes/MessageSender.pm
+++ Hermes/MessageSender.pm
@@ -56,8 +56,19 @@ use constant SQL => scalar "SELECT gn.id, gn.notification_id, gn.created_at, sub
  WHERE gn.sent = 0 AND subs.enabled=1 AND subs.delay_id=?\
  ORDER BY subs.id, gn.created_at DESC LIMIT ?";
 
+use constant QUERY_SUBS => scalar "SELECT distinct subs.id FROM generated_notifications gn \
+  JOIN subscriptions subs ON subs.id = gn.subscription_id WHERE gn.sent = 0 AND subs.enabled=1 \
+  AND subs.delay_id=?";
+
+use constant QUERY_DIGEST => scalar "SELECT gn.id, gn.notification_id, gn.created_at, \
+ subs.msg_type_id, subs.person_id, subs.delay_id, subs.delivery_id FROM \
+ generated_notifications gn\
+ JOIN subscriptions subs ON subs.id = gn.subscription_id\
+ WHERE gn.sent = 0 AND subs.enabled=1 AND subs.id=?\
+ ORDER BY gn.created_at DESC LIMIT ?";
+
 @ISA	    = qw(Exporter);
-@EXPORT	    = qw( sendMessageDigest sendImmediateMessages );
+@EXPORT	    = qw( sendOneMessageDigest sendImmediateMessages getDigestSubscribtions );
 
 
 ######################################################################
@@ -102,21 +113,21 @@ sub markSent( $ )
 
 =head1 NAME
 
-sendMessageDigest() - sends a digest of queued messages
+getDigestSubscribtions - return a list of subscribtions for a specific delay
 
 =head1 SYNOPSIS
 
     use Hermes::MessageSender;
 
-    my $delay = SEND_HOURLY;	# Send messages queued for hourly distribution.
-    my $type = 'test';		# Send messages of the 'test' type.
+    my $delay = SEND_HOURLY;    # Send messages queued for hourly distribution.
 
-    my $count = sendMessageDigest($delay, $type);
+    my $list = getDigestSubscribtions($delay);
 
 =head1 DESCRIPTION
 
-sendMessageDigest() sends a digest of similarly-typed messages in the 
-system queue.  Each message is marked according to its distribution frequency
+getDigestSubscribtions() gives you a list of ids that you can pass to
+sendOneMessageDigest.
+Each message is marked according to its distribution frequency
 (e.g. hourly, daily, weekly, monthly) and its type.  Messages with the same
 type will have their message bodies grouped into a single digested mail.
 
@@ -124,10 +135,47 @@ type will have their message bodies grouped into a single digested mail.
 
 Parameter 1 indicates the frequency of this digest run.
 
-Parameter 2 is an optional parameter that specifies the message type for
-this mailing.
+=head1 RETURN VALUE
 
-Parameter 3 is an optional debug flag.
+Returns a reference to an array of ids
+
+=cut
+
+sub getDigestSubscribtions($)
+{
+  my ($delay) = @_;
+
+  # Find all messages of the same type and delay that haven't been sent.
+  log('notice', "Fetching subscribtions of all types with delay $delay");
+
+  my $query = dbh()->prepare( QUERY_SUBS );
+  $query->execute( $delay );
+  my @ret = ();
+  foreach my $row (@{$query->fetchall_arrayref([0])}) {
+    push(@ret, shift @$row);
+  }
+  return \@ret;
+}
+
+
+=head1 NAME
+
+sendOneMessageDigest() - sends a digest of queued messages
+
+=head1 SYNOPSIS
+
+    use Hermes::MessageSender;
+
+    my $count = sendOneMessageDigest($subscribtion);
+
+=head1 DESCRIPTION
+
+sendMessageDigest() sends a digest of similarly-typed messages in the 
+system queue. It requires to know the subscribtion id
+
+=head1 PARAMETERS
+
+Parameter 1 subscribtion id
 
 =head1 RETURN VALUE
 
@@ -135,9 +183,9 @@ Returns the number of messages sent in this digest run.
 
 =cut
 
-sub sendMessageDigest($;)
+sub sendOneMessageDigest($;)
 {
-  my ($delay, $subject) = @_;
+  my ($subscription) = @_;
 
   # Find all messages of the same type and delay that haven't been sent.
   my $sth;
@@ -146,58 +194,27 @@ sub sendMessageDigest($;)
   my @handledIds;
   my @msg;
 
-  log('notice', "Fetching messages of all types with delay $delay");
+  $query = dbh()->prepare( QUERY_DIGEST );
+  $query->execute( $subscription, 300 );
 
-  $query = dbh()->prepare( SQL ) unless( $query );
-  $query->execute( $delay, 1000 );
-
-  my $currentPerson;
+  my $delayString = "";
   my $currentDelivery;
-  my $currentType;
-
-  my $delayString = delayIdToString( $delay );
   my $renderedRef;
   my $summedBody = "";
   my @genNotiIds;
   my @toc;
   my $cnt = 1;
 
-  while( my( $genNotiId, $notiId, $genNotiCreated, $subscriptId, $msgTypeId, $personId,
+  while( my( $genNotiId, $notiId, $genNotiCreated, $msgTypeId, $personId,
 	     $delayId, $deliveryId ) = $query->fetchrow_array() ) {
 
-    # set sensible start values if the current- values are undefined.
-    $currentPerson   = $personId unless( $currentPerson );
-    $currentDelivery = $deliveryId unless( $currentDelivery );
-    $currentType     = $msgTypeId unless( $currentType );
+    $delayString = delayIdToString( $delayId );
+    $currentDelivery = $deliveryId;
 
-    log( 'info', "Current Person $personId <=> $currentPerson" );
-    log( 'info', "Current Delivery $deliveryId <=> $currentDelivery" );
-    log( 'info', "Current Type $msgTypeId <=> $currentType" );
-
-    if( $currentPerson   != $personId  ||
-	$currentType     != $msgTypeId ||
-	$currentDelivery != $deliveryId ) {
-      # This means that the collected content needs to be sent because
-      # the receiver changed or the receiver is the same but the delivery
-      # is different or the message type has changed.
-      my $s = $renderedRef->{subject};
-      $renderedRef->{subject} = sprintf( "[digest %s] %d messages", $delayString, $cnt  );
-      $renderedRef->{subject} .= ", eg. $s" if( $s );
-
-      if( sendSummedMessage( $renderedRef, $summedBody, $currentDelivery ) ) {
-	markSent( \@genNotiIds );
-	@genNotiIds = ();
-      }
-      $summedBody = "";
-      @toc = ();
-      $cnt = 1;
-    } else {
-      # all parameters are still fine, we continue to collect gen_notification details
-    }
     # query the parameter hash
     my $paramHash = getGeneratedNotificationParameters( $notiId );
     # render the message and get the digest text out.
-    $renderedRef = renderMessage( $msgTypeId, $notiId, $subscriptId, $personId,
+    $renderedRef = renderMessage( $msgTypeId, $notiId, $subscription, $personId,
 				  $delayId, $deliveryId, $paramHash );
 
     unless( $renderedRef->{body} ) {
@@ -221,12 +238,9 @@ sub sendMessageDigest($;)
       # no digest sektion, append the whole text without signature
       my $sumBody = $renderedRef->{body};
       $sumBody =~ s/^-- \R.*$//sm;
-      log('info', "Add to summed up body: $sumBody" );
+      log('debug', "Add to summed up body: $sumBody" );
       $summedBody .= $sumBody;
     }
-    $currentPerson = $personId;
-    $currentDelivery = $deliveryId;
-    $currentType = $msgTypeId;
     push @genNotiIds, $genNotiId; # Needed to mark the notifications sent
     push @handledIds, $notiId;
     $cnt++;
@@ -244,7 +258,7 @@ sub sendMessageDigest($;)
       markSent( \@genNotiIds );
     }
   }
-  return \@handledIds;
+  return $query->rows > 299 ? 0 : 1 ;
 }
 
 sub sendSummedMessage( $$$$ )
@@ -438,8 +452,8 @@ sub sendImmediateMessages(;$)
   my ($type) = @_;
 
   my $cnt = 0;
-  $query = dbh()->prepare(SQL) unless( $query );
-  $query->execute( SendNow(), 1000 );
+  $query = dbh()->prepare(SQL);
+  $query->execute( SendNow(), 300 );
 
   while( my( $genNotiId, $notiId, $genNotiCreated, $subscriptId, $msgTypeId,
 	     $personId, $delayId, $deliveryId ) = $query->fetchrow_array() ) {
